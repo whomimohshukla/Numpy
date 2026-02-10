3476acf551fe0455fc935227e38589eca72253da
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-11 04:45:51 +0530

    Add astype.py to demonstrate type conversion of NumPy arrays

---
+++ NumPy-properties/astype.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+
+arr=np.array([1,2,3])
+print(arr)
+
+# convert to float
+float_arr=arr.astype(float)
+print(float_arr)
+
+# convert to int
+int_arr=arr.astype(int)
+print(int_arr)
+
