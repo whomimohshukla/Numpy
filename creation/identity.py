55ab255f93380720e0b8cec82a677c563882ce41
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-11 04:33:38 +0530

    Add examples for creating sequences and identity arrays in numpy

---
+++ creation/identity.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+identity_array=np.identity(5)
+print(identity_array)
---
+++ creation/sequences.py
@@ -0,0 +1,7 @@
+# creating sequences of numbers in numpy
+# arange() function is used to create sequences of numbers in numpy. It takes three arguments: start, stop, and step. The start argument is the starting value of the sequence, the stop argument is the ending value of the sequence, and the step argument is the increment between each value in the sequence.
+
+import numpy as np
+
+arr=np.arange(1,10,2)
+print(arr)
