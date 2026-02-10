f8cacfa0922757aeaf7e5d56b81fe7b4a94147cf
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-11 04:44:32 +0530

    Add ndim.py to demonstrate the dimensionality of NumPy arrays

---
+++ NumPy-properties/ndim.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr_1d=np.array([1,2,3])
+print(arr_1d.ndim)
+arr_2d=np.array([[1,2,3],[4,5,6]])
+print(arr_2d.ndim)
+
+arr_3d=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(arr_3d.ndim)
