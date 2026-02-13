f8ff20f15d3b3a5c593615b20a2401d1069ac399
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-14 02:53:40 +0530

    Add indexing&slicing.py to demonstrate indexing and slicing of NumPy arrays

---
+++ indexing/indexing&slicing.py
@@ -0,0 +1,13 @@
+# one D array
+
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6,7,8,9,10])
+
+# print(arr[0])
+# print(arr[1:3])
+
+arr2=np.array([[1,2,3],[4,5,6]])
+
+print(arr2[0,1])
+print(arr2[:,1])
