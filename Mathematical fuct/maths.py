1209c5566941ea09439a386a56f5a801a9a2b477
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-14 02:53:58 +0530

    Add maths.py to demonstrate various mathematical operations using NumPy

---
+++ Mathematical fuct/maths.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+a=np.array([1,2,3,4,5,6,7,8,9,10])
+
+print(np.sqrt(a))
+print(np.exp(a))
+print(np.log(a))
+print(np.log10(a))
+print(np.sin(a))
+print(np.cos(a))
+print(np.tan(a))
+print(np.arcsin(a))
+print(np.arccos(a))
+print(np.arctan(a))
