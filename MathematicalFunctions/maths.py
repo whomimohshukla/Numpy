9522c8e823a55f4dc584ad2e4ba8dbd95e87d4dd
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-20 23:40:22 +0530

    Add maths.py file with basic NumPy mathematical functions demonstrations

---
+++ MathematicalFunctions/maths.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+a = np.array([1,2,3,4,5,6,7,8,9,10])
+angles = np.array([-1.0, -0.5, 0.0, 0.5, 1.0])  # valid domain for arcsin/arccos
+
+print(np.sqrt(a))
+print(np.exp(a))
+print(np.log(a))
+print(np.log10(a))
+print(np.sin(a))
+print(np.cos(a))
+print(np.tan(a))
+print(np.arcsin(angles))
+print(np.arccos(angles))
+print(np.arctan(a))
